f53a44da44b7a90534442894fa6ff98f88c4701f
Kim, JiHee <rlagus1614@gmail.com>
2021-01-27 17:35:22 +0900

    libft.h 수정

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -30,20 +30,20 @@ typedef struct	s_list
 **	Part 1 - Libc functions
 */
 
-void			*ft_memset(void *s, int c, size_t n);
-void			ft_bzero(void *s, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
-void			*ft_memccpy(void *dests, const void *src, int c, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memchr(const void *s, int c, size_t n);
+void			*ft_memset(void *b, int c, size_t n);
+void			ft_bzero(void *ptr, size_t len);
+void			*ft_memcpy(void *dst, const void *src, size_t len);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t len);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memchr(const void *str, int c, size_t len);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t			ft_strlen(const char *s);
-size_t			ft_strlcpy(char *dest, const char *src, size_t size);
-size_t			ft_strlcat(char *dest, const char *src, size_t size);
+size_t			ft_strlen(const char *str);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strlcat(char *dst, const char *src, size_t n);
 char			*ft_strchr(const char *str, int c);
-char			*ft_strrchr(const char *str, int c);
-char			*ft_strnstr(const char *big, const char *little, size_t len);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *str, const char *find, size_t len);
+int				ft_strncmp(const char *s1, const char *s2, size_t num);
 int				ft_atoi(const char *str);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
