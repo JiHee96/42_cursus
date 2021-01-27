0fe1d166d5962abf780ed64fb335e93bc10d830d
Kim, JiHee <rlagus1614@gmail.com>
2021-01-27 16:51:05 +0900

    수정

--- libft.a/ft_atoi.c
+++ libft.a/ft_atoi.c
@@ -12,41 +12,42 @@
 
 #include "libft.h"
 
-int ft_space(char c)
+int		is_space(char c)
 {
-    if ((c == ' ' || c == '\t' || c == '\n' ||
+	if (c == ' ' || c == '\t' || c == '\n' ||
 			c == '\f' || c == '\v' || c == '\r')
-            return (1);
-    return (0);            
+		return (1);
+	return (0);
 }
 
-int ft_digit(char c)
+int		is_digit(char c)
 {
-    if (c <= '9' && c > '0')
-        return (1);
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
 
-int ft_atoi(const char  *str)
+int		ft_atoi(const char *str)
 {
-    int result;
-    int sign;
+	int		i;
+	int		num;
+	int		flag;
 
-    result  = 0;
-    sign = 1;
-    while (ft_space(*str))
-        str++;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            sign = -1;
-        ++str;
-    }
-    whlie (ft_digit(*str))
-    {
-        result *= 10;
-        result += *str - '0';
-        ++str;
-    }
-    return (num * sign);
+	flag = 1;
+	num = 0;
+	i = 0;
+	while (is_space(str[i]))
+		i++;
+	if (str[i] == '-')
+		flag = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && is_digit(str[i]))
+	{
+		num *= 10;
+		num += str[i] - '0';
+		i++;
+	}
+	num *= flag;
+	return (num);
 }
