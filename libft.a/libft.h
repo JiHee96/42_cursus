63cf47b6a4dbecc9d45e0b3bb54de8f8019729da
Kim, JiHee <rlagus1614@gmail.com>
2021-01-27 17:44:10 +0900

    2

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -58,7 +58,7 @@ char			*ft_strdup(const char *str);
 /*
 **	Part 2 - Additional Functions
 */
-
+/*
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
@@ -73,7 +73,7 @@ void			ft_putnbr_fd(int n, int fd);
 /*
 **	Bonus part - Linked list
 */
-
+/*
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
@@ -83,5 +83,5 @@ void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *l, void *(*f)(void *), void (*d)(void *));
-
+*/
 #endif
