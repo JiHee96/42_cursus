3001bb81f9303ddbe38167b15ce59f6f73f1e7ee
Jihee Kim <jihekim@c2r8s4.42seoul.kr>
2021-01-28 15:59:42 +0900

    11

--- libft.a/ft_memcpy.c
+++ libft.a/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: jihekim <jihekim@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 22:08:10 by jihekim           #+#    #+#             */
-/*   Updated: 2021/01/13 22:08:10 by jihekim          ###   ########.fr       */
+/*   Updated: 2021/01/28 15:59:22 by jihekim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
     unsigned char       *dstpoint;
     const unsigned char *srcpoint;
