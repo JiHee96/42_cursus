662d07e11e1bccd46e7f997f8855751c04587f30
Kim, JiHee <rlagus1614@gmail.com>
2021-01-29 21:29:18 +0900

    14

--- libft.a/ft_strlcat.c
+++ libft.a/ft_strlcat.c
@@ -12,23 +12,26 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t len)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	size_t	destlen;
 	size_t	srclen;
-	size_t	dstlen;
 
+	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
-	dstlen = ft_strlen(dst);
-	if (dstlen > len)
-		dstlen = len;
-	if (dstlen == len || len == 0)
-		return (dstlen + srclen);
-	if (srclen < len - dstlen)
-		ft_memcpy(dst + dstlen, src, srclen + 1);
+	if (destlen > size)
+		destlen = size;
+	if (destlen == size)
+		return (size + srclen);
+	if (srclen < size - destlen)
+	{
+		ft_memcpy(dest + destlen, src, srclen);
+		dest[destlen + srclen] = '\0';
+	}
 	else
 	{
-		ft_memcpy(dst + dstlen, src, len - dstlen - 1);
-		dst[len - 1] = '\0';
+		ft_memcpy(dest + destlen, src, size - destlen - 1);
+		dest[size - 1] = '\0';
 	}
-	return (dstlen + srclen);
+	return (destlen + srclen);
 }
